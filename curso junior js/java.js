d28c5552d71dca335af82ff79e535e8bf89113b9
Sebita1809 <157644469+Sebita1809@users.noreply.github.com>
2024-04-02 21:16:16 -0300

    j

--- curso junior js/java.js
+++ curso junior js/java.js
@@ -1,1 +1,69 @@
-prompt("decime tu nombre");
+// dineroCofla = prompt("cuanto dinero tienes Cofla?");
+// dineroRoberto = prompt("cuanto dinero tienes Roberto?");
+// dineroPedro = prompt("cuanto dinero tienes Pedro?");
+
+// if(dineroCofla >= 0.6 && dineroCofla < 1){
+//     alert("Cofla, te alcanza para un helado de agua");
+// } else if (dineroCofla >= 1 && dineroCofla < 1.6){
+//     alert("Cofla, te alcanza para un helado de crema");
+// } else if (dineroCofla >= 1.6 && dineroCofla < 1.7){
+//     alert("Cofla, te alcanza para helado de heladix");
+// } else if (dineroCofla >= 1.7 && dineroCofla < 1.8){
+//     alert("Cofla, te alcanza para un helado heladovich");
+// } else if (dineroCofla >= 1.8 && dineroCofla < 2.6){
+//     alert("Cofla, te alcanza para un helado helardo");
+// } else if (dineroCofla >= 2.6){
+//     alert("Cofla, te alcanza para un helado con confites o para 1/4kg");
+// } else {
+//     alert("Cofla, no te alcanza para una poronga, pobre de mierda");
+// }
+// if(dineroRoberto >= 0.6 && dineroRoberto < 1){
+//     alert("Roberto, te alcanza para un helado de agua");
+// } else if (dineroRoberto >= 1 && dineroRoberto < 1.6){
+//     alert("Roberto, te alcanza para un helado de crema");
+// } else if (dineroRoberto >= 1.6 && dineroRoberto < 1.7){
+//     alert("Roberto, te alcanza para helado de heladix");
+// } else if (dineroRoberto >= 1.7 && dineroRoberto < 1.8){
+//     alert("Rberto, te alcanza para un helado heladovich");
+// } else if (dineroRoberto >= 1.8 && dineroRoberto < 2.6){
+//     alert("Roberto, te alcanza para un helado helardo");
+// } else if (dineroRoberto >= 2.6){
+//     alert("Roberto, te alcanza para un helado con confites o para 1/4kg");
+// } else {
+//     alert("Roberto, no te alcanza para una poronga, pobre de mierda");
+// }
+// if(dineroPedro >= 0.6 && dineroPedro < 1){
+//     alert("Pedro, te alcanza para un helado de agua");
+// } else if (dineroPedro>= 1 && dineroPedro < 1.6){
+//     alert("Pedro, te alcanza para un helado de crema");
+// } else if (dineroPedro >= 1.6 && dineroPedro < 1.7){
+//     alert("Pedro, te alcanza para helado de heladix");
+// } else if (dineroPedro >= 1.7 && dineroPedro < 1.8){
+//     alert("Pedro, te alcanza para un helado heladovich");
+// } else if (dineroPedro >= 1.8 && dineroPedro < 2.6){
+//     alert("Pedro, te alcanza para un helado helardo");
+// } else if (dineroPedro >= 2.6){
+//     alert("Pedro, te alcanza para un helado con confites o para 1/4kg");
+// } else {
+//     alert("Pedro, no te alcanza para una poronga, pobre de mierda");
+// }
+///////////////////////////////////////////////////////////////////////
+// let usuario = {
+//     nombre: "Sebita",
+//     edad: "20",
+//     nacionalidad: "argentino",
+//     documento: 45257656
+// };
+
+// let nombre = usuario["nombre"];
+// let edad = usuario["edad"];
+// let nacionalidad = usuario["nacionalidad"];
+// let documento = usuario["documento"];
+
+// let frase = `Mi nombre es: ${nombre} <br>
+//              Mi edad es: ${edad} <br>
+//              Nacionalidad: ${nacionalidad} <br>
+//              Mi documento es: ${documento}
+// `
+// document.write(frase);
+//////////////////////////////////////////////////////////////////////
---
+++ curso junior js/variables.js
@@ -0,0 +1,11 @@
+dineroCofla = "3";
+dineroCofla = parseInt(dineroCofla); // ESTA FUNCION SIRVE PARA PASAR ESE 3 (EN FORMATO STRING) A UN 3 EN FORMATO NUMERO//
+
+let usuario = {
+    nombre: "Sebita",
+    edad: "20",
+    nacionalidad: "argentino",
+    documento: 45257656
+}; //ESTO ES UN ARRAY ASOCIATIVO//
+
+
